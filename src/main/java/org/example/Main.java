bd8651af4582b94eff3f54b6284e8d99771194fe
mahdimazaheri <mahdi.mazaheri8383@gmail.com>
2024-05-13 16:54:37 +0330

    add sum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,4 +30,8 @@ public class Main {
             System.out.println(colorQueue.poll() + person.getName());
         });
     }
+
+    public static int sum(int x, int y) {
+        return 0;
+    }
 }
