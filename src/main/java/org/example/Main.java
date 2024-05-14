0a9b4c7f2917336303e86bd2b8b3ae0bfadc5eec
Zahra <Zmsmart2020@gmail.com>
2024-05-14 15:30:02 +0330

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,12 +14,12 @@ public class Main {
     public static void main(String[] args) {
         Person mahdiMazaheri = new Person("Mahdi Mazaheri 🔥🐢");
         // Construct your person object below
-
+Person zahra=new Person("zahra Mahdavi ✨");
 
         // ----------------------------------------
         personArrayList.add(mahdiMazaheri);
         personArrayList.addAll(Arrays.asList(
-                // Add your person object here
+                zahra
         ));
         printResultToTerminal();
     }
